776a03669569b191a7e80fababe15c3062c16c66
Oleksandr Nahirniak <alexander.nagirnyak@gmail.com>
2021-02-08 22:14:08 +0200

    comment tests stages

--- 2020/19/python/message_verifier_test.py
+++ 2020/19/python/message_verifier_test.py
@@ -7,19 +7,18 @@ import os.path
 class MessageVerifierTests(unittest.TestCase):
 
     def test_given_by_platform_input(self):
-        
+        # Arrange
         message_reader = MessageReader()
-
-        messages = message_reader.read_messages(os.path.dirname(__file__) + '/data/messages_testcase_1.txt')
-
         rules_reader = RulesReader()
 
+        messages = message_reader.read_messages(os.path.dirname(__file__) + '/data/messages_testcase_1.txt')
         rules = rules_reader.read_rules(os.path.dirname(__file__) + '/data/rules_testcase_1.txt')
 
+        # Act
         verifier = MessageVerifier()
-
         validMessages = verifier.verify_messages(rules, messages)
 
+        # Assert
         self.assertEqual(validMessages, 285)
 
 
