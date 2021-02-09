f55b3a1b61f2f597bc5bf25f0f2824425ffbd10f
Oleksandr Nahirniak <alexander.nagirnyak@gmail.com>
2021-02-09 16:28:15 +0200

    implement part 2

--- 2020/19/python/message_verifier.py
+++ 2020/19/python/message_verifier.py
@@ -2,12 +2,21 @@ import collections
 
 class MessageVerifier:
 
-    def verify_messages(self, rules, messages):
+    def verify_messages_part_two(self, rules, messages):
+
+        messages_from_42 = self.__backtrack_messages(rules, '42') 
+        messages_from_31 = self.__backtrack_messages(rules, '31')
+
+        filtered_messages = self.__filter_messages(messages, messages_from_42, messages_from_31)
+
+        return len(filtered_messages)
+
+    def verify_messages_part_one(self, rules, messages):
 
         # I can use Trie here for optimizations of memory
-        intersected = self.__backtrack_messages(rules, '0') & messages 
+        messages_to_verify = self.__backtrack_messages(rules, '0') 
 
-        return len(intersected)
+        return len(messages_to_verify & messages)
     
     def __backtrack_messages(self, rules, current_rule):
         # Base case
@@ -21,7 +30,6 @@ class MessageVerifier:
             for next_rule in branch_of_rules:
 
                 downstream_candidates = self.__backtrack_messages(rules, next_rule) 
-                
                 '''
                 Combine the previous calculated candidates with the next ones:
 
@@ -38,5 +46,39 @@ class MessageVerifier:
                     candidates = downstream_candidates
 
             result |= candidates
+
+        return result
+
+    def __filter_messages(self, total_messages, messages_from_42, messages_from_31):
+        result = []
+        
+        # REFACTOR
+        for message in total_messages:
+            if len(message) % 8 != 0: continue
+            if message[:8] not in messages_from_42: continue
+            if message[-8:] not in messages_from_31: continue
+
+            isAllBlocksValid = True
+            message_blocks_in_42 = 0
+            message_blocks_in_31 = 0
+            for i in range(0, len(message), 8):
+                message_block = message[i:i + 8]
+
+                if message_block in messages_from_42:
+                    message_blocks_in_42 += 1
+                if message_block in messages_from_31:
+                    message_blocks_in_31 += 1
+
+                if message_block in messages_from_31:
+                    if len(message) - i >= 16:
+                        next_message_block =  message_block = message[i + 8:i + 16]
+                        if next_message_block in messages_from_42:
+                            isAllBlocksValid = False
+                            break
+
+            if not isAllBlocksValid: continue
+            if message_blocks_in_42 <= message_blocks_in_31: continue
+            
+            result.append(message)
 
         return result
--- 2020/19/python/message_verifier_test.py
+++ 2020/19/python/message_verifier_test.py
@@ -6,7 +6,8 @@ import os.path
 
 class MessageVerifierTests(unittest.TestCase):
 
-    def test_given_by_platform_input(self):
+
+    def test_given_by_platform_input_part_1(self):
         # Arrange
         message_reader = MessageReader()
         rules_reader = RulesReader()
@@ -16,11 +17,28 @@ class MessageVerifierTests(unittest.TestCase):
 
         # Act
         verifier = MessageVerifier()
-        validMessages = verifier.verify_messages(rules, messages)
+        validMessages = verifier.verify_messages_part_one(rules, messages)
 
         # Assert
         self.assertEqual(validMessages, 285)
 
+    def test_given_by_platform_input_part_2(self):
+        # Arrange
+        message_reader = MessageReader()
+        rules_reader = RulesReader()
+
+        messages = message_reader.read_messages(os.path.dirname(__file__) + '/data/messages_testcase_1.txt')
+        rules = rules_reader.read_rules(os.path.dirname(__file__) + '/data/rules_testcase_1.txt')
+
+        rules['8'] = [(42), (42, 8)]
+        rules['11'] = [(42, 31), (42, 11, 31)]
+
+        # Act
+        verifier = MessageVerifier()
+        validMessages = verifier.verify_messages_part_two(rules, messages)
+
+        # Assert
+        self.assertEqual(validMessages, 412)
 
 if __name__ == '__main__':
     unittest.main() 
